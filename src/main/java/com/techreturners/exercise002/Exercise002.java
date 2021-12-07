f6e93ceef800e748399241ab6c85bff007c163b8
sairaawan <syraawan@live.com>
2021-12-07 12:32:16 +0000

    excercise002 complete

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,15 +1,48 @@
 package com.techreturners.exercise002;
 
+class Person{
+    String fname;
+    String lname;
+    String city;
+    int age;
+    public String getFname()
+   {
+     return fname;
+   }
+ 
+   
+   public String getLname()
+   {
+     return lname;
+   }
+ 
+   public String getcity()
+   {
+     return city;
+   }
+   public int getAge()
+   {
+     return age;
+   }
+    public Person(String fnameVal, String lnameVal, String cityVal, int ageVal)
+        {
+           fname = fnameVal;
+           lname = lnameVal;
+           city=cityVal;
+           age = ageVal;
+        }
+ 
+ }
+
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person p) {
+       return p.getcity().equalsIgnoreCase("manchester");
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person p, int ageLimit) {
+        return p.getAge()>=ageLimit;
+
     }
     
 }
